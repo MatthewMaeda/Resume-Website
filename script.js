78167e0b511c34fb869b0e527e081079af8e5cdc
Matthew Maeda <matthewmaeda@Matthews-MacBook-Pro.local>
2020-01-17 14:09:39 -0500

    changes layout

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("sideNavID").style.width = "300px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("sideNavID").style.width = "0";
-}
 
